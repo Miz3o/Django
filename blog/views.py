72de0c57f10888a3685458a9c1bb612898548117
Miz3o <121382956+Miz3o@users.noreply.github.com>
2022-12-25 13:58:37 +0200

    My first app

---
+++ blog/views.py
@@ -0,0 +1,20 @@
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView # новое изменение
+
+from .models import Post
+
+
+class BlogListView(ListView):
+    model = Post
+    template_name = 'home.html'
+
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+
+
+class BlogCreateView(CreateView): # новое изменение
+    model = Post
+    template_name = 'post_new.html'
+    fields = ['title', 'author', 'body']
